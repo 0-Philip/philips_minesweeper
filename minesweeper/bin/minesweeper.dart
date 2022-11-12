9c32cf2f78d1eae3ede6e3c27f4984714fca6f4b
Philip Keller <philipugk@gmail.com>
2022-11-12 14:03:35 +0100

    created project

---
+++ minesweeper/bin/minesweeper.dart
@@ -0,0 +1,5 @@
+import 'package:minesweeper/minesweeper.dart' as minesweeper;
+
+void main(List<String> arguments) {
+  print('Hello world: ${minesweeper.calculate()}!');
+}
---
+++ minesweeper/test/minesweeper_test.dart
@@ -0,0 +1,8 @@
+import 'package:minesweeper/minesweeper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
