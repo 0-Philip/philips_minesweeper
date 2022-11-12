4cb0bbd1720d03848feda9fb3b51256d196f5ce8
Philip Keller <philipugk@gmail.com>
2022-11-12 15:11:16 +0100

    played around with printing

---
+++ minesweeper/bin/sketchpad.dart
@@ -0,0 +1,20 @@
+import "dart:io";
+
+void main() {
+  print("hi!");
+
+  const matrixSize = 9;
+
+  List<List<String?>> matrix = [
+    ["d", null, null, "e"],
+    [null, "█", null, "🍅"],
+    ["😉", null, "🦡", "█"],
+  ];
+
+  for (var row in matrix) {
+    for (var cell in row) {
+      stdout.write(cell ?? "░");
+    }
+    stdout.writeln('');
+  }
+}
