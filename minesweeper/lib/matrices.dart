c3ff1050bc34172deb73cc5964dbb25bfff30fcf
Philip Keller <philipugk@gmail.com>
2022-11-13 14:23:33 +0100

    fixed off by one error in createMatrix function

--- minesweeper/lib/matrices.dart
+++ minesweeper/lib/matrices.dart
@@ -1,8 +1,8 @@
 List<List<T?>> createMatrix<T>(int matrixSize) {
   List<List<T?>> matrix = [
-    for (var i = 0; i <= matrixSize; i++)
+    for (var i = 0; i < matrixSize; i++)
       [
-        for (var i = 0; i <= matrixSize; i++) null,
+        for (var i = 0; i < matrixSize; i++) null,
       ],
   ];
   return matrix;
