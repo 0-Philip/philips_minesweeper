dc83c4ded93f7e2def62a6ff36bd48ebca01c632
Philip Keller <philipugk@gmail.com>
2022-11-14 15:36:31 +0100

    add create neighbours functionality

--- minesweeper/lib/minesweeper.dart
+++ minesweeper/lib/minesweeper.dart
@@ -10,17 +10,50 @@ int calculate() {
 class Minesweeper {
   Minesweeper({int fieldSize = 9, int mineCount = 10})
       : minefield = createMatrix<CellBase>(fieldSize),
-        mines = [for (var i = 0; i < mineCount; i++) Mine(0, 0)];
+        mines = [for (var i = 0; i < mineCount; i++) Mine(0, 0)],
+        xOuterBound = fieldSize,
+        yOuterBound = fieldSize;
 
   List<List<CellBase?>> minefield;
   List<Mine> mines;
+  final int xOuterBound;
+  final int yOuterBound;
 
   void initialize() {
     scatter(mines, throughoutField: minefield);
+    addNeighbours();
+    printMineswithDebug();
+  }
 
+  void addNeighbours() {
+    for (var mine in mines) {
+      var xStart = mine.position.x;
+      var yStart = mine.position.y;
+
+      for (var i = xStart - 1; i <= xStart + 1; i++) {
+        if ((i < xOuterBound) && (i >= 0)) {
+          for (var j = yStart - 1; j <= yStart + 1; j++) {
+            if ((j < yOuterBound) && (j >= 0)) {
+              if (minefield[j][i] == null) {
+                minefield[j][i] = NumberedCell(i, j);
+              } else {
+                minefield[j][i]!.increment();
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  void printMineswithDebug() {
     for (var row in minefield) {
       for (var cell in row) {
-        stdout.write(cell == null ? "░" : "@");
+        if (cell is NumberedCell) {
+          stdout.write(cell.adjacentMineCount);
+        } else {
+          stdout.write(cell == null ? "░" : "@");
+        }
       }
       stdout.writeln('');
     }
---
+++ minesweeper/test/generate_neighbours_test.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+import 'package:minesweeper/cells.dart';
+import 'package:minesweeper/matrices.dart';
+
+var mines = [
+  Mine(2, 1),
+  Mine(1, 3),
+  Mine(3, 3),
+];
+
+var minefield = createMatrix<CellBase>(4);
+
+var xOuterBound = 4;
+var yOuterBound = 4;
+
+void addNeighbours() {
+  for (var mine in mines) {
+    var xStart = mine.position.x;
+    var yStart = mine.position.y;
+
+    for (var i = xStart - 1; i <= xStart + 1; i++) {
+      if ((i < xOuterBound) && (i >= 0)) {
+        for (var j = yStart - 1; j <= yStart + 1; j++) {
+          if ((j < yOuterBound) && (j >= 0)) {
+            if (minefield[j][i] == null) {
+              minefield[j][i] = NumberedCell(i, j);
+            } else {
+              minefield[j][i]!.increment();
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void printMineswithDebug() {
+  for (var row in minefield) {
+    for (var cell in row) {
+      if (cell is NumberedCell) {
+        stdout.write(cell.adjacentMineCount);
+      } else {
+        stdout.write(cell == null ? "░" : "@");
+      }
+    }
+    stdout.writeln('');
+  }
+
+  for (var mine in mines) {
+    print("My coordinates are ${mine.position.x},${mine.position.y}");
+  }
+}
+
+void main() {
+  for (var mine in mines) {
+    minefield[mine.position.y][mine.position.x] = mine;
+  }
+
+  addNeighbours();
+  printMineswithDebug();
+}
