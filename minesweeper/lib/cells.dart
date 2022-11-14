db6ea14d1cce078a318c245541da8b7066e556e6
Philip Keller <philipugk@gmail.com>
2022-11-14 15:35:58 +0100

    added increment method in cells

--- minesweeper/lib/cells.dart
+++ minesweeper/lib/cells.dart
@@ -7,16 +7,32 @@ class Position {
 abstract class CellBase {
   Position position;
   CellBase(int x, int y) : position = Position(x, y);
+  void increment();
 }
 
 class Mine extends CellBase {
   Mine(super.x, super.y);
+  @override
+  void increment() {
+    // do nothing
+  }
 }
 
 class NumberedCell extends CellBase {
-  NumberedCell(super.x, super.y);
+  int adjacentMineCount;
+  NumberedCell(super.x, super.y) : adjacentMineCount = 1;
+
+  @override
+  void increment() {
+    adjacentMineCount++;
+  }
 }
 
 class EmptyCell extends CellBase {
   EmptyCell(super.x, super.y);
+
+  @override
+  void increment() {
+    assert(false, "empty cells should not be incremented");
+  }
 }
