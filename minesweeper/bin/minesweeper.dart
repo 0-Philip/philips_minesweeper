9cbe77391a6b39fa6b2e38e7ee33a5e7758fdc8d
Philip Keller <philipugk@gmail.com>
2022-11-14 12:48:38 +0100

    cleaned up placholder in main

--- minesweeper/bin/minesweeper.dart
+++ minesweeper/bin/minesweeper.dart
@@ -1,5 +1,3 @@
 import 'package:minesweeper/minesweeper.dart' as minesweeper;
 
-void main(List<String> arguments) {
-  print('Hello world: ${minesweeper.calculate()}!');
-}
+void main(List<String> arguments) {}
