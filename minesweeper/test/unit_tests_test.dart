8064c7699c5664d1952649efe32e6b8f363806b3
Philip Keller <philipugk@gmail.com>
2023-07-12 22:09:23 +0200

    updated unit tests

--- minesweeper/test/unit_tests_test.dart
+++ minesweeper/test/unit_tests_test.dart
@@ -1,11 +1,22 @@
-import 'dart:math';
-
 import 'package:minesweeper/cells.dart';
 import 'package:minesweeper/minesweeper.dart';
 import 'package:test/test.dart';
 
 void main() {
   test("a test to test how tests test", () => expect(true, !false));
+
+  test("minecount correct", () {
+    var minesweeper = Minesweeper(fieldSize: 9, mineCount: 10);
+    minesweeper.initialize();
+
+    expect(
+      minesweeper.minefield
+          .map((row) => row.countType<Mine>())
+          .reduce((value, element) => value + element),
+      10,
+    );
+  });
+
   test('the numbers are correctly placed', () {
     var minesweeper = Minesweeper(fieldSize: 9, mineCount: 10);
     minesweeper.initialize();
@@ -18,12 +29,7 @@ void main() {
           forEachSurrounding(x, y, (i, j) {
             neighbours.add(minesweeper.minefield.tryGet(j)?.tryGet(i));
           });
-          expect(
-            cell.adjacentMineCount,
-            neighbours.fold(0, (mineCount, element) {
-              return element is Mine ? mineCount + 1 : mineCount;
-            }),
-          );
+          expect(cell.adjacentMineCount, neighbours.countType<Mine>());
         }
       }
     }
@@ -38,6 +44,8 @@ void forEachSurrounding(int x, int y, void Function(int, int) function) {
   }
 }
 
-extension ListGetExtension<T> on List<T> {
+extension ListExtension<T> on List<T> {
   T? tryGet(int index) => index < 0 || index >= length ? null : this[index];
+  int countType<U>() =>
+      fold(0, (tCount, element) => element is U ? tCount + 1 : tCount);
 }
